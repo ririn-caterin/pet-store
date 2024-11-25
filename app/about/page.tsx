74335e31174f881a53ecd29ceed830eaa8ab43e1
Ririn <caterin.ririn@gmail.com>
2024-11-25 12:22:24 +0700

    create homepage and navigation

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-10">ABOUT PAGE</div>
+    </div>
+  );
+};
+
+export default Page;
